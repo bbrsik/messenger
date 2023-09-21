71ab19c57c2ea8adc8771aeeb88657f0c34c65a7
bbrsik <sainte66@gmail.com>
2023-09-21 20:33:26 +0300

    save point before messing with the database

--- message/models.py
+++ message/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
--- test_request.py
+++ test_request.py
@@ -1,4 +1,4 @@
 import requests
 
 
-requests.post("http://127.0.0.1:8000/message/create/", {"message": "text"})
+requests.post("http://127.0.0.1:8000/message/create/", {"test message": "test text"})
