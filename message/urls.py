6ba9a7e65a8fdce4f2a6986ae6343227853d5e76
bbrsik <sainte66@gmail.com>
2023-11-13 19:53:27 +0300

    hotfix

--- message/urls.py
+++ message/urls.py
@@ -15,8 +15,6 @@ urlpatterns = [
 
         path('chat/', include([
             path("create/", api_views.create_chat, name='create_chat'),
-            path("show/<chat_id>/", api_views.show_chat, name='show_chat'),
-            path("list/", api_views.list_chats, name='list_chats'),
         ])),
     ])),
 ]
