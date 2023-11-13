8cb138ca81067074fe810f29125cd598d0bca4b6
bbrsik <sainte66@gmail.com>
2023-11-13 17:58:44 +0300

    begin message-delete

---
+++ message/api_views.py
@@ -0,0 +1,64 @@
+import urllib
+import json
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from message.serializers import *
+
+
+@csrf_exempt
+def login_view(request):
+    body = json.loads(request.body)
+    user = authenticate(request, username=body.get('username'), password=body.get('password'))
+    if not user:
+        return JsonResponse({}, status=403)
+    login(request, user=user)
+    return JsonResponse({})
+
+
+@csrf_exempt
+def create_message(request, chat_id):
+    if request.method != "POST":
+        return JsonResponse({}, status=400)
+    body = urllib.parse.parse_qs(request.body.decode())
+    [message] = body.get('message')
+    chat = chat_id
+    try:
+        Chat.objects.get(id=chat)
+    except Chat.DoesNotExist:
+        return JsonResponse({'Chat does not exist.'}, status=400)
+
+    msg = Message(text=message, user=request.user, chat_id=chat)
+    msg.save()
+    return redirect(reverse("render_chat", kwargs={'chat_id': chat_id}))
+
+
+@csrf_exempt
+def create_chat(request):
+    if request.method == "POST":
+        body = urllib.parse.parse_qs(request.body.decode())
+        [name] = body.get('name')
+        chat = Chat(name=name)
+        chat.save()
+    return redirect(reverse("render_list"))
+
+
+def show_chat(request, chat_id):
+    if request.method != "GET":
+        return JsonResponse({}, status=400)
+
+    try:
+        Chat.objects.get(id=chat_id)
+    except Chat.DoesNotExist:
+        return JsonResponse({'Response': 'Chat does not exist'}, status=404)
+    messages = serialize_messages(Message.objects.filter(chat_id=chat_id))
+    return JsonResponse({'messages': messages})
+
+
+def list_chats(request):
+    if request.method != "GET":
+        return JsonResponse({}, status=400)
+    chats = serialize_chats(Chat.objects.all())
+    return JsonResponse({'chats': chats})
--- message/urls.py
+++ message/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from . import views, api_views
 
 urlpatterns = [
     path("chat/<chat_id>/", views.render_chat, name='render_chat'),
     path("chats/", views.render_list, name='render_list'),
 
-    path("api/login/", views.login_view, name='login'),
-    path("api/message/create/<chat_id>/", views.create_message, name='create_message'),
-    path("api/chat/create/", views.create_chat, name='create_chat'),
-    path("api/chat/show/<chat_id>/", views.show_chat, name='show_chat'),
-    path("api/chat/list/", views.list_chats, name='list_chats'),
+    path('api/', include([
+        path("login/", api_views.login_view, name='login'),
+        path("message/create/<chat_id>/", api_views.create_message, name='create_message'),
+
+        path('chat/', include([
+            path("create/", api_views.create_chat, name='create_chat'),
+            path("show/<chat_id>/", api_views.show_chat, name='show_chat'),
+            path("list/", api_views.list_chats, name='list_chats'),
+        ])),
+    ])),
 ]
