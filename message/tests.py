487eabb28c77692054851bc5a73d199727b2001a
unknown <sainte66@gmail.com>
2023-11-27 19:39:31 +0300

    tests

--- message/tests.py
+++ message/tests.py
@@ -1,28 +1,69 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
+from message.models import Chat, Message
+from message.serializers import serialize_chats, serialize_messages
+from message.api_views import create_chat
 
-from message.models import Chat
-from message.serializers import serialize_chats
+
+class SerializersTestCase(TestCase):
+    def setUp(self):
+        # serialize_chats test setup
+        self.chat = Chat.objects.create(
+            id=1,
+            name='Test chat'
+        )
+
+        # serialize_messages test setup
+        self.user1 = User.objects.create(username='user1')
+        self.user2 = User.objects.create(username='user2')
+        self.message1 = Message.objects.create(
+            user=self.user1,
+            chat=self.chat,
+            text='text1',
+        )
+        self.message2 = Message.objects.create(
+            user=self.user2,
+            chat=self.chat,
+            text='text2',
+            reply_on=self.message1,
+        )
+
+    def test_serialize_chats(self):
+        serialized_chat = serialize_chats(Chat.objects.all())[0]
+        self.assertEqual(serialized_chat['id'], 1),
+        self.assertEqual(serialized_chat['name'], 'Test chat'),
+        self.assertEqual(serialized_chat['created_at'], self.chat.created_at.strftime("%D %H:%M:%S")),
+
+    def test_serialize_messages(self):
+        serialized_message1 = serialize_messages(Message.objects.all())[0]
+        self.assertEqual(serialized_message1['username'], 'user1'),
+        self.assertEqual(serialized_message1['text'], 'text1'),
+        self.assertEqual(serialized_message1['created_at'], self.message1.created_at.strftime("%D %H:%M:%S")),
+
+        serialized_message2 = serialize_messages(Message.objects.all())[1]
+        self.assertEqual(serialized_message2['username'], 'user2'),
+        self.assertEqual(serialized_message2['text'], 'text2'),
+        self.assertEqual(serialized_message2['replies_history'],
+                         serialize_messages(self.message2.get_replies_history())),
+        self.assertEqual(serialized_message2['created_at'], self.message2.created_at.strftime("%D %H:%M:%S")),
 
 
-class MessageAppTestCase(TestCase):
+class ApiViewsTestCase(TestCase):
+    def setUp(self):
+        # create_chat test
+        pass
+    pass
+
+
+class RenderViewsTestCase(TestCase):
     def setUp(self):
         self.chat = Chat.objects.create(
             id=1,
             name='Test chat'
         )
 
-    def testChatSerializer(self):
-        serialized_chat = serialize_chats(Chat.objects.all())[0]
-        self.assertEqual(serialized_chat['id'], 1),
-        self.assertEqual(serialized_chat['name'], 'Test chat'),
-        self.assertEqual(serialized_chat['created_at'], self.chat.created_at.strftime("%D %H:%M:%S")),
-
-    def testMessageSerializer(self):
-        # todo test
-        pass
-
-    def testRenderChatView(self):
+    def test_render_chat(self):
         url = reverse('render_chat', kwargs={'chat_id': 1})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
@@ -30,4 +71,9 @@ class MessageAppTestCase(TestCase):
         response = self.client.get(url2)
         self.assertEqual(response.status_code, 404)
 
-    # todo написать максимально большое кол-во тестов
+        # smoke test
+
+    def test_render_list(self):
+        url = reverse('render_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
